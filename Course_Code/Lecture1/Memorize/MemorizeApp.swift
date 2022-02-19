2328b28f61bd6ad9b7af60a52287627c7b936c3f
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-02-19 17:09:30 +0900

    Lecture 1 생성

---
+++ Course_Code/Lecture1/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jinyoung Yoo on 2022/02/19.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
