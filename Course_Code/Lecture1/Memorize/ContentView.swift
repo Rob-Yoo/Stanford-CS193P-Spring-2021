296dbaee5d0b33838c591c7a785e25040672f100
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-02-22 04:19:21 +0900

    Feat : Lecture 1

--- Course_Code/Lecture1/Memorize/ContentView.swift
+++ Course_Code/Lecture1/Memorize/ContentView.swift
@@ -9,10 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .center) {
+            // view builder
+            //return a bag of legos(Tuple Views) for the ZStack to work on
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+            Text("Hello,world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
